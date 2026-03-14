83ef7278205cdcc177903bfdfd39c21aa3a0e0d1
1rfxn <legendarybasha786@gmail.com>
2026-03-14 15:30:37 +0530

    Initial commit

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -8,6 +8,7 @@ public class PrimeNumber {
             {
                 if(i % j == 0)
                 {  
+                    
                     count++;
                     break;
                 }
